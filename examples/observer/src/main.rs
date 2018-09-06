c10409b243363b38b980fec2f6c4ffd4fc51c6f4
Louis Dureuil <louis.dureuil@xinra.net>
2018-09-06 23:02:32 +0200

    Version with multiple observers

--- examples/observer/src/main.rs
+++ examples/observer/src/main.rs
@@ -33,43 +33,56 @@ impl Observer {
 }
 
 struct Visitable {
-    observer: Sc<Observer>,
+    observers: Vec<Sc<Observer>>,
 }
 
 impl Visitable {
-    pub fn new() -> Self {
-        Visitable {
-            observer: Sc::new(),
+    pub fn new(observer_count: usize) -> Self {
+        let mut v = Vec::new();
+        v.reserve(observer_count);
+        for _i in 0..observer_count {
+            v.push(Sc::new());
         }
+        Visitable { observers: v }
     }
 
     pub fn add_log(&self, log: &str) {
-        match self.observer.get() {
-            Some(observer) => {
-                println!("Notifying {}", &observer.name);
-                observer.notify(log)
+        for observer in &self.observers {
+            match observer.get() {
+                Some(observer) => {
+                    println!("Notifying {}", &observer.name);
+                    observer.notify(log)
+                }
+                None => {}
             }
-            None => {
-                println!("No one to notify for log: {}!", log);
-            }
-        };
+        }
     }
 
     pub fn register_observer<'observer, 'sc>(
         &'sc self,
         observer: &'observer Observer,
-    ) -> Dropper<'observer, 'sc, Observer> {
-        self.observer.set(&observer)
+    ) -> Option<Dropper<'observer, 'sc, Observer>> {
+        let sc = self
+            .observers
+            .iter()
+            .find(|observer| observer.get().is_none())?;
+        Some(sc.set(observer))
     }
 }
 
 fn main() {
-    let visitable = Visitable::new();
+    let visitable = Visitable::new(10);
     visitable.add_log("Lost for science!");
     {
         let observer = Observer::new(String::from("Toto"));
-        let _dropper = visitable.register_observer(&observer);
+        let _dropper = visitable.register_observer(&observer).unwrap();
         visitable.add_log("Registered log");
+        {
+            let observer = Observer::new(String::from("Titi"));
+            let _dropper = visitable.register_observer(&observer).unwrap();
+            visitable.add_log("a second one");
+            observer.print();
+        }
         visitable.add_log("and another one!");
         observer.print();
     }
