c27d1fd258bafa88bb99e44a95f3382339705436
Louis Dureuil <louis.dureuil@xinra.net>
2018-09-20 21:08:39 +0200

    Add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,126 @@
+//!
+//! The `sc` module provides the `Sc` type, an "Almost-Safe" reference type with a dynamic lifetime.
+//!
+//! A `Sc<T>` is similar to a `&'a T`, but can be stored in a struct without tying the `'a` lifetime to the struct.
+//! In exchange for that, a dynamic check is performed to ensure that the reference is still alive when it is accessed.
+//!
+//! # Examples
+//!
+//! A `Sc<T>` is always empty upon initialization.
+//! ```rust
+//! use sc::Sc;
+//!
+//! let sc = Sc::<String>::new();
+//! assert!(sc.is_none()); // `Sc` is initially empty.
+//! ```
+//!
+//! To set a `Sc` to reference something, one must create a `Locker` object.
+//! The `Locker` object is responsible for keeping the lifetime of the referenced object.
+//! To tie a `Sc` to a reference, one must call the `Locker::lock` method on a `Locker` object.
+//! ```rust
+//! use sc::Sc;
+//! use sc::Locker;
+//!
+//! let sc = Sc::new();
+//! assert!(sc.is_none()); // `Sc` is initially empty.
+//! {
+//!     let s = String::from("foo");
+//!     let mut locker = Locker::new(); // create a `Locker` object.
+//!     unsafe { locker.lock(&s, &sc) }; // This call sets the sc to reference `s`
+//!     assert!(sc.is_some()); // `Sc` now contains something!
+//! }
+//! ```
+//!
+//! The `Sc` remains tied to the reference for the **entire lifetime** of the `Locker` instance,
+//! then it becomes empty again.
+//!
+//! ```rust
+//! use sc::Sc;
+//! use sc::Locker;
+//!
+//! let sc = Sc::new();
+//! assert!(sc.is_none()); // `Sc` is initially empty.
+//! {
+//!     let s = String::from("foo");
+//!     let mut locker = Locker::new(); // create a `Locker` object.
+//!     unsafe { locker.lock(&s, &sc) }; // This call sets the sc to reference `s`
+//!     assert!(sc.is_some()); // `Sc` now contains something!
+//! }
+//! assert!(sc.is_none()); // empty again!
+//! ```
+//!
+//! While the `Sc` is set to a reference, the reference can be accessed in a closure through the `map` method.
+//! ```rust
+//! use sc::Sc;
+//! use sc::Locker;
+//!
+//! let sc = Sc::new();
+//! assert!(sc.is_none()); // `Sc` is initially empty.
+//! {
+//!     let s = String::from("foo");
+//!     let mut locker = Locker::new(); // create a `Locker` object.
+//!     unsafe { locker.lock(&s, &sc) }; // This call sets the sc to reference `s`
+//!     assert!(sc.is_some()); // `Sc` now contains something!
+//!     sc.map(|x| println!("{}", x)); // will print "foo"
+//! }
+//! sc.map(|x| println!("{}", x)); // does nothing
+//! ```
+//!
+//! Similarly to `Option::map`, if the closure returns a value it will be wrapped in an option that will be `Some` if
+//! the `Sc` is set to a reference, and `None` otherwise.
+//!
+//! ```rust
+//! use sc::Sc;
+//! use sc::Locker;
+//!
+//! let sc = Sc::new();
+//! assert!(sc.is_none()); // `Sc` is initially empty.
+//! {
+//!     let s = String::from("foo");
+//!     let mut locker = Locker::new(); // create a `Locker` object.
+//!     unsafe { locker.lock(&s, &sc) }; // This call sets the sc to reference `s`
+//!     assert!(sc.is_some()); // `Sc` now contains something!
+//!     assert_eq!(sc.map(|x| x.clone() + "bar"), Some(String::from("foobar")));
+//! }
+//! assert_eq!(sc.map(|x| x.clone() + "bar"), None);
+//! ```
+//!
+//! Here is a simple use of a `Sc` to store a reference to a `String` in a struct `A` that doesn't have a lifetime:
+//!  ```rust
+//! use sc::Sc;
+//! use sc::Locker;
+//!
+//! struct A { // No declared lifetime!
+//!    sc : Sc<String>,
+//! }
+//!
+//! let a = A { sc : Sc::new() }; // `Sc` is initially empty.
+//! assert!(a.sc.is_none());
+//! {
+//!     let s = String::from("foo");
+//!     let mut locker = Locker::new();
+//!     unsafe { locker.lock(&s, &a.sc) }; // set `Sc` to reference the String `s`.
+//!     assert!(a.sc.is_some());
+//!     a.sc.map(|x| println!("{}", x)); // use the reference: prints "foo"
+//! }
+//! assert!(a.sc.is_none());
+//! ```
+//!
+//! # Safety
+//!
+//! `Sc` relies on the `drop` method being called on the `Locker` instance to be safe. However,
+//! leaking a value is considered safe is rust. As a result, the `Locker::lock` method is marked unsafe.
+//! Note that it is statically impossible to call `Locker::lock` on a `Locker` instance in a `Rc` cycle, which
+//! notably decreases the risk of accidental leaks.
+//! See the `Locker::lock` method documentation for further details.
+//!
+
 use std::cell::Cell;
 
+/// An "Almost-Safe" reference type with a dynamic lifetime.
+///
+/// `Sc` stands for `Schroedinger` because one must check to know if the reference is dead or alive.
+/// See the [crate-level documentation](./index.html) for more details.
 pub struct Sc<T: ?Sized>(Cell<Option<*const T>>);
 
 struct Dropper<'sc, T: ?Sized + 'sc> {
@@ -14,6 +135,9 @@ impl<'sc, T: ?Sized> Drop for Dropper<'sc, T> {
 
 struct Marker;
 
+/// The object responsible for the lifetime of the reference set in `Sc`.
+///
+/// See the [crate-level documentation](./index.html) for more details.
 pub struct Locker<'sc, 'auto, T: ?Sized + 'sc + 'auto> {
     sc: Option<Dropper<'sc, T>>,
     marker: Marker,
@@ -21,6 +145,7 @@ pub struct Locker<'sc, 'auto, T: ?Sized + 'sc + 'auto> {
 }
 
 impl<'sc, 'auto, T: ?Sized> Locker<'sc, 'auto, T> {
+    /// Constructs a new `Locker<T>`.
     pub fn new() -> Self {
         Self {
             sc: None,
@@ -29,6 +154,29 @@ impl<'sc, 'auto, T: ?Sized> Locker<'sc, 'auto, T> {
         }
     }
 
+    /// Lock the passed reference `val` with the passed `sc` for the **entire lifetime** of this `Locker`.
+    ///
+    /// # Safety
+    ///
+    /// This method is unsafe, because the passed `sc` relies on `drop` being called for the `Locker` instance,
+    /// and leaking an object without calling `drop` is considered safe in Rust.
+    ///
+    /// To prevent any possible access to a dangling reference with the `Sc::map` method,
+    /// this `Locker` instance must verify the following preconditions before calling `Locker::lock`:
+    ///   * It must not be wrapped in a `ManuallyDrop` wrapper.
+    ///   * It must not be put in a Box that is leaked.
+    ///   * It must not be put in a cycle (like a `Rc` cycle). Fortunately, this is impossible to do using safe Rust.
+    ///   * (once Drop objects in enum land in stable) It must not be a member of a `union`
+    ///
+    /// # Examples
+    ///
+    /// Because it is set for the entire lifetime of the `Locker`, it is statically impossible to `lock` a `Locker`
+    /// to a reference of a shorter lifetime.
+    ///
+    /// Note that it is impossible call `Locker::lock()` on a locker in a cycle.
+    ///
+    /// Note that it is also impossible to call `mem::forget()` on a locker instance for which `Locker::lock()` has been called.
+    ///
     pub unsafe fn lock(&'auto mut self, val: &'auto T, sc: &'sc Sc<T>) {
         let ptr = val as *const T;
         sc.0.set(Some(ptr));
